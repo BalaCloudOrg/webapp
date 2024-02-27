e29969348dfc090048771d0d8ad28099bf9a6ac9
Balasubramanian U <ubalasubramanian03@gmail.com>
2024-02-27 15:03:41 -0500

    a5-major commit

--- packer/scripts/image-setup-script.sh
+++ packer/scripts/image-setup-script.sh
@@ -6,52 +6,86 @@ set -e
 # 1. Update the packages
 sudo dnf update -y
 
-# 2. Install the mysql-server
-# echo "y" | sudo dnf install mysql-server
-
-# 3. Start the mysql service
-# sudo systemctl start mysqld.service
-
-# 4. Check the status of the mysql service
-# sudo systemctl status mysqld
-
-# 5. Verify mysql command execution as root user
-# mysqladmin -u root version
-
-# 6. MYSQL setup
-# mysql -u root <<EOF
-# CREATE DATABASE IF NOT EXISTS test;
-# CREATE USER 'user1'@'localhost' IDENTIFIED BY 'Abcd@123';
-# GRANT ALL PRIVILEGES ON test.* TO 'user1'@'localhost';
-# FLUSH PRIVILEGES;
-# EOF
-
-
-# 7. Set environment variables in .bashrc
-# cat << EOF >> ~/.bashrc
-
-# config for webapp
-# export PORT="3000"
-# export MYSQL_HOST="localhost"
-# export MYSQL_USER="user1"
-# export MYSQL_PASSWORD="Abcd@123"
-# export MYSQL_DATABASE="test"
-# EOF
-
-# 8. Source .bashrc (Note: Sourcing in a script will not affect the parent shell)
-# source ~/.bashrc
-
-# 8. Check available Node.js versions
+# 2. Check available Node.js versions
 sudo dnf module list nodejs
 
-# 9. Enable the latest Node.js module version available before 21
+# 3. Enable the latest Node.js module version available before 21
 sudo dnf module enable -y nodejs:20
 
-# 10. Install Node.js
+# 4. Install Node.js
 sudo dnf install -y nodejs
 
-# 11. Check Node.js version
+# 5. Check Node.js version
 node --version
 
-# 12. Check npm version
+# 6. Check npm version
 npm --version
+
+# 7. install unzip
+sudo dnf install -y unzip
+
+# 8. Create user and group
+sudo groupadd csye6225
+sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+
+# 9. Unzip the application code
+sudo unzip /tmp/test-src.zip -d /opt/test-src
+
+# 10. Navigate into the folder and install dependencies
+cd /opt/test-src
+npm i || true
+echo "completed npm i"
+npm i bcrypt
+echo "completed npm i brcrypt"
+
+# 11. Navigate back to the home directory
+cd ~
+
+# 12. Change ownership and set executable permissions
+sudo chown -R csye6225:csye6225 /opt/test-src
+
+# sudo chown -R csye6225:csye6225 /opt/config.env
+
+# 13. Setting executable permissions
+# For directories: grant read, write, and execute permissions to user and group
+# For files: grant read and execute permissions to user and group
+sudo find /opt/test-src -type d -exec chmod 750 {} \;
+# sudo find /opt/test-src -type f -exec chmod 640 {} \;
+
+# If you also want to ensure that all files are executable by the user and group, change the file permission line to:
+sudo find /opt/test-src -type f -exec chmod 750 {} \;
+
+# 14. Create the systemd service file
+# Environment=MYSQL_HOST="localhost"
+# Environment=MYSQL_USER="user1"
+# Environment=MYSQL_PASSWORD="Abcd@123"
+# Environment=MYSQL_DATABASE="test"
+sudo tee /etc/systemd/system/webapp-service.service << 'EOF'
+[Unit]
+Description=CSYE 6225 App
+After=network.target google-startup-scripts.service
+ConditionPathExists=/opt/config.env
+
+[Service]
+Environment=PORT="3000"
+EnvironmentFile=/opt/config.env
+Type=simple
+User=csye6225
+Group=csye6225
+WorkingDirectory=/opt/test-src/
+ExecStart=/usr/bin/node index.js
+Restart=always
+RestartSec=3
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=csye6225
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+# 15. Reload systemd and enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable webapp-service.service
+sudo systemctl start webapp-service.service
