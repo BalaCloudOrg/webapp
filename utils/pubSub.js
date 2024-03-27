e5d01a4e57d30d8d12f9daaced43b6ff1882afbc
Balasubramanian U <ubalasubramanian03@gmail.com>
2024-03-27 08:37:01 -0400

    A7 - Clean up and few corrections

--- utils/pubSub.js
+++ utils/pubSub.js
@@ -1,19 +1,13 @@
-// Import the PubSub class
 const { PubSub } = require("@google-cloud/pubsub");
 const logger = require("./logging");
 
 const pubSub = new PubSub();
 
 async function publishMessage(topicName, data) {
-  // Creates a client; credentials will be taken from the environment variables by default
-  // References an existing topic
-
   try {
-    // Publishes a message
-    const dataBuffer = Buffer.from(JSON.stringify(data));
     const messageId = await pubSub
       .topic(topicName)
-      .publishMessage({ data: dataBuffer });
+      .publishMessage({ data: JSON.stringify(data) });
     logger.info(`Message ${messageId} published to ${topicName}`, data);
   } catch (error) {
     logger.error(`Error publishing message to ${topicName}`, { error: error });
